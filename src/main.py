49f57cc4edae8e1abeba8ce7c4bac1193703fb04
nlina-0 <ngolina0@gmail.com>
2024-05-07 00:35:03 +1000

    user account sign up and log in set up + vitamins list written out

--- src/main.py
+++ src/main.py
@@ -1,37 +1,40 @@
 from user_acc import signup
 from user_acc import login
+import sys
+from vitamins import vitamin_list
 # import user_acc // why can't do this?
 
 # initial sign up, login or quit
-print('********** Login System **********')
-print('\n1.Signup')
-print('2.Login')
-# print("3.Exit")
-ch = int(input('\nEnter your choice: '))
-if ch == 1:
-    signup()
-elif ch == 2:
-    login()
-else:
-    print('Invalid Choice!')
+while True:
+    print('********** Login System **********')
+    print('\n1.Signup')
+    print('2.Login')
+    print('3.Exit')
+    ch = int(input('\nEnter your choice: '))
+    if ch == 1:
+        signup()
+        break
+    elif ch == 2:
+        login()
+        break
+    elif ch == 3:
+        sys.exit("Goodbye!")
+    else:
+        print('Invalid Choice!')
 
 
-# 1. select vitamin to track
-# 2. view history
+# option to track new vitamin or view history
+print('\n********** Options **********')
+print('\n1.Select vitamin to track')
+# select a date or view all
+print('2.View history') 
+print('3.Exit')
+ch2 = int(input('\nEnter your choice: '))
+if ch2 == 1:
+    # output vitamin selection
+    vitamin_list()
+elif ch2 == 2:
+    pass
+elif ch2 == 3:
+    sys.exit("Goodbye!")
 
-
-
-# vitamin selection
-print('\n********** Vitamin Selection **********')
-
-# improve this list seleciton
-vitamins =  [
-    {1: '1. Fish Oil'},
-    {2: '2. Magnesium'},
-    {3: '3. Vitamin C'}
-]
-for vit in vitamins:
-    print(list(vit.values()))
-
-print('\nWhat would you like to track? Please enter your choice:')
-# how to print text?
---
+++ src/vitamins.py
@@ -0,0 +1,13 @@
+def vitamin_list():
+    print('\n********** Vitamin Selection **********\n')
+    vitamins = ['Fish Oil', 'Magnesium', 'Iron', 'Vitamin C', 'H2O']
+
+    for key, value in enumerate(vitamins):
+        print(f'{key + 1}. {value}')
+
+    print('\nWhat would you like to track? Enter the number:')
+    
+    # match number thing
+
+# Are you taking any supplments for ____ ?
+# How many mg are you taking? 
